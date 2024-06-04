9a146390f9f393ba43673e2739cde15ebc2d1699
Fernando Souza <fernando.dev22@gmail.com>
2024-06-04 06:39:19 -0300

    Funções para deletar um usuário e menu de escolhas

--- crud básico.py
+++ crud básico.py
@@ -36,3 +36,19 @@ def atualizar_usuario(id, nome, idade):
     cursor.execute('''UPDATE usuarios SET nome = ?, idade = ? WHERE id = ?''', (nome, idade, id))
     conexao.commit()
     conexao.close()
+
+# Função para deletar um usuário
+def deletar_usuario(id):
+    conexao = sqlite3.connect('exemplo.db')
+    cursor = conexao.cursor()
+    cursor.execute('''DELETE FROM usuarios WHERE id = ?''', (id,))
+    conexao.commit()
+    conexao.close()
+
+# Função do menu de escolhas
+def menu():
+    print("\n1. Adicionar usuário")
+    print("2. Listar usuários")
+    print("3. Atualizar usuário")
+    print("4. Deletar usuário")
+    print("5. Sair")
