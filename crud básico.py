13ea63577582e710e6926c31eca5d1bad4d1d821
Fernando Souza <fernando.dev22@gmail.com>
2024-06-04 16:27:38 -0300

    Criar uma nova tabela***

--- crud básico.py
+++ crud básico.py
@@ -52,3 +52,34 @@ def menu():
     print("3. Atualizar usuário")
     print("4. Deletar usuário")
     print("5. Sair")
+
+# Criação de uma nova tabela***
+criar_tabela()
+
+while True:
+    menu()
+    escolha = input("Escolha uma opção: ")
+
+    if escolha == '1':
+        nome = input("Digite o nome do usuário: ")
+        idade = int(input("Digite a idade do usuário: "))
+        adicionar_usuario(nome, idade)
+        print("Usuário adicionado com sucesso!")
+    elif escolha == '2':
+        print("\nTodos os usuários:")
+        listar_usuarios()
+    elif escolha == '3':
+        id = int(input("Digite o ID do usuário a ser atualizado: "))
+        nome = input("Digite o novo nome do usuário: ")
+        idade = int(input("Digite a nova idade do usuário: "))
+        atualizar_usuario(id, nome, idade)
+        print("Usuário atualizado com sucesso!")
+    elif escolha == '4':
+        id = int(input("Digite o ID do usuário a ser deletado: "))
+        deletar_usuario(id)
+        print("Usuário deletado com sucesso!")
+    elif escolha == '5':
+        print("Saindo do programa...")
+        break
+    else:
+        print("Opção inválida. Por favor, escolha uma opção válida.")
