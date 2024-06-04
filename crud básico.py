d236d7f5a811e180aa743e9b2221dd396982d159
Fernando Souza <fernando.dev22@gmail.com>
2024-06-03 21:33:22 -0300

    Função para adicionar usuários

--- crud básico.py
+++ crud básico.py
@@ -10,3 +10,11 @@ def criar_tabela():
                         idade INTEGER)''')
     conexao.commit()
     conexao.close()
+
+# Função para adicionar um novo usuário na tabela
+def adicionar_usuario(nome, idade):
+    conexao = sqlite3.connect('exemplo.db')
+    cursor = conexao.cursor()
+    cursor.execute('''INSERT INTO usuarios (nome, idade) VALUES (?, ?)''', (nome, idade))
+    conexao.commit()
+    conexao.close()
